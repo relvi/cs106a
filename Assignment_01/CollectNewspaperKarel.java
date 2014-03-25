d7a5edacfb81cadf4c6753b8b20cefc5189cf325
relvi <relvi@mail.com>
2014-03-25 14:27:05 +0000

    Changed tabs to spaces.

--- Assignment_01/CollectNewspaperKarel.java
+++ Assignment_01/CollectNewspaperKarel.java
@@ -9,58 +9,57 @@
 import stanford.karel.*;
 
 public class CollectNewspaperKarel extends SuperKarel {
-	public void run() {
-		moveToNewspaper();
-		pickUpNewspaper();
-		returnToStartPoint();
-	}
-	
-	/**
-	 * Moves Karel to the newspaper.
-	 * This method is only valid on worlds with the same dimensions and layout
-	 * as the world shown in the diagram.
-	 */
-	private void moveToNewspaper() {
-		moveToWall();
-		turnRight();
-		move();
-		turnLeft();
-		move();
-	}
-	
-	/**
-	 * Picks up the newspaper (beeper), if present.
-	 * When pickUpNewspaper is called, Karel must be on the same square as the
-	 * newspaper.
-	 */
-	private void pickUpNewspaper() {
-		if (beepersPresent()) {
-			pickBeeper();
-		}
-	}
-	
-	/**
-	 * Moves Karel back to its starting position.
-	 * This method is only valid on worlds with the same dimensions and layout
-	 * as the world shown in the diagram.
-	 */
-	private void returnToStartPoint() {
-		turnAround();
-		move();
-		turnRight();
-		move();
-		
-		turnLeft();
-		moveToWall();
-		turnAround();
-	}
-	
-	/**
-	 * Moves Karel until it is blocked by a wall.
-	 */
-	private void moveToWall() {
-		while (frontIsClear()) {
-			move();
-		}
-	}
+    public void run() {
+        moveToNewspaper();
+        pickUpNewspaper();
+        returnToStartPoint();
+    }
+
+    /**
+     * Moves Karel to the newspaper.
+     * This method is only valid on worlds with the same dimensions and layout
+     * as the world shown in the diagram.
+     */
+    private void moveToNewspaper() {
+        moveToWall();
+        turnRight();
+        move();
+        turnLeft();
+        move();
+    }
+
+    /**
+     * Picks up the newspaper (beeper), if present.
+     * When pickUpNewspaper is called, Karel must be on the same square as the
+     * newspaper.
+     */
+    private void pickUpNewspaper() {
+        if (beepersPresent()) {
+            pickBeeper();
+        }
+    }
+
+    /**
+     * Moves Karel back to its starting position.
+     * This method is only valid on worlds with the same dimensions and layout
+     * as the world shown in the diagram.
+     */
+    private void returnToStartPoint() {
+        turnAround();
+        move();
+        turnRight();
+        move();
+        turnLeft();
+        moveToWall();
+        turnAround();
+    }
+
+    /**
+     * Moves Karel until it is blocked by a wall.
+     */
+    private void moveToWall() {
+        while (frontIsClear()) {
+            move();
+        }
+    }
 }
